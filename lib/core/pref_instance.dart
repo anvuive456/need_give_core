3855d1bf24248125bca1f05e85786c43a19e70f3
anvuive456 <trannhatan2803@gmail.com>
2022-12-13 06:36:48 +0700

    comment pref instance

--- lib/core/pref_instance.dart
+++ lib/core/pref_instance.dart
@@ -1,10 +1,10 @@
-import 'package:need_give_core/core/app_constants.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-
-class PrefInstance {
-
-  saveAccessToken(String accessToken) async {
-    var prefs = await SharedPreferences.getInstance();
-    prefs.setString(PrefKey.PREF_ACCESS_TOKEN, accessToken);
-  }
-}
+// import 'package:need_give_core/core/app_constants.dart';
+// import 'package:shared_preferences/shared_preferences.dart';
+//
+// class PrefInstance {
+//
+//   saveAccessToken(String accessToken) async {
+//     var prefs = await SharedPreferences.getInstance();
+//     prefs.setString(PrefKey.PREF_ACCESS_TOKEN, accessToken);
+//   }
+// }
