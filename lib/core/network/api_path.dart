f741c957e8b8f4a9a7777eda0ef1d824b778ad0e
anvuive456 <trannhatan2803@gmail.com>
2022-12-12 15:53:24 +0700

    update user update path

--- lib/core/network/api_path.dart
+++ lib/core/network/api_path.dart
@@ -9,17 +9,17 @@ class ApiPath {
 
   //auth
   static const String sign_in = BASE_URL + API + V + '/auth/sign-in';
-  static const String sign_up_donor = BASE_URL + API + V + '/auth/donor/sign-up';
-  static const String sign_up_needer = BASE_URL + API + V + '/auth/needer/sign-up';
-
+  static const String sign_up_donor =
+      BASE_URL + API + V + '/auth/donor/sign-up';
+  static const String sign_up_needer =
+      BASE_URL + API + V + '/auth/needer/sign-up';
 
   //charity
   static String charityByUserId(int id) =>
       BASE_URL + API + V + '/user/${id}/charity';
   static const String charity = BASE_URL + API + V + '/charity';
 
-
   //user
-  static const String update_profile = BASE_URL+ API+'/user';
+  static const String update_profile = BASE_URL + API + V + '/user';
   static const String profile = BASE_URL + API + V + '/user/me';
 }
