bbb9ff153af06925f537bf1a243441b4e24689fa
anvuive456 <trannhatan2803@gmail.com>
2022-12-18 20:48:44 +0700

    update media to json

--- lib/core/model/media_model.dart
+++ lib/core/model/media_model.dart
@@ -1,30 +1,42 @@
-
 class Media {
+  final int id;
   final String filePath;
   final String fileName;
   final int fileSize;
   final String type;
+
   factory Media.fromJson(Map<String, dynamic> json) {
     return Media(
+        id: json['id'] ?? -1,
         filePath: json['filePath'],
         fileName: json['fileName'],
         type: json['type'],
         fileSize: json['size']);
   }
+
   Map<String, dynamic> get toJson {
-    return {'filePath': filePath, 'fileName': fileName, 'size': fileSize};
+    return {
+      'id': id,
+      'filePath': filePath,
+      'fileName': fileName,
+      'size': fileSize,
+      'type': 'image',
+    };
   }
 
   const Media.empty(
       {this.fileName = '',
-        this.filePath = '',
-        this.fileSize = 0,
-        this.type = ''});
+      this.filePath = '',
+      this.fileSize = 0,
+      this.id = -1,
+      this.type = ''});
+
   Media({
     required this.filePath,
     required this.fileName,
     required this.type,
     required this.fileSize,
+    required this.id,
   });
 
   Media copyWith({
