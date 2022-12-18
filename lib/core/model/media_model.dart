2bf72f94dea48206f6d13b80115351da1c3ab037
anvuive456 <trannhatan2803@gmail.com>
2022-12-18 18:25:47 +0700

    add media model

---
+++ lib/core/model/media_model.dart
@@ -0,0 +1,43 @@
+
+class Media {
+  final String filePath;
+  final String fileName;
+  final int fileSize;
+  final String type;
+  factory Media.fromJson(Map<String, dynamic> json) {
+    return Media(
+        filePath: json['filePath'],
+        fileName: json['fileName'],
+        type: json['type'],
+        fileSize: json['size']);
+  }
+  Map<String, dynamic> get toJson {
+    return {'dial_code': filePath, 'country': fileName, 'phone': fileSize};
+  }
+
+  const Media.empty(
+      {this.fileName = '',
+        this.filePath = '',
+        this.fileSize = 0,
+        this.type = ''});
+  Media({
+    required this.filePath,
+    required this.fileName,
+    required this.type,
+    required this.fileSize,
+  });
+
+  Media copyWith({
+    String? filePath,
+    String? fileName,
+    String? type,
+    int? fileSize,
+  }) {
+    return Media(
+      filePath: filePath ?? this.filePath,
+      type: type ?? this.type,
+      fileName: fileName ?? this.fileName,
+      fileSize: fileSize ?? this.fileSize,
+    );
+  }
+}
