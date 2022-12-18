aec187522a4e676db5e74c941c47aab2170ea9cd
anvuive456 <trannhatan2803@gmail.com>
2022-12-18 18:35:47 +0700

    update media path

--- lib/core/network/api_path.dart
+++ lib/core/network/api_path.dart
@@ -19,18 +19,23 @@ class ApiPath {
       BASE_URL + API + V + '/user/${id}/charity';
   static const String charity = BASE_URL + API + V + '/charity';
 
-  static String charityByRequestId(int id)=>
-  BASE_URL + API +V +'/request/${id}/charity';
-
+  static String charityByRequestId(int id) =>
+      BASE_URL + API + V + '/request/${id}/charity';
 
   //request
-  static const String request = BASE_URL +API +V +'/request';
+  static const String request = BASE_URL + API + V + '/request';
+
   static String requestByUserId(int id) =>
       BASE_URL + API + V + '/user/${id}/request';
 
-  static const String requestForDonor = BASE_URL + API +V +'/donor/request';
+  static const String requestForDonor = BASE_URL + API + V + '/donor/request';
 
   //user
   static const String update_profile = BASE_URL + API + V + '/user';
   static const String profile = BASE_URL + API + V + '/user/me';
+
+  //upload media
+  static const String upload_single_image =
+      BASE_URL + API + V + '/media/single';
+  static const String upload_multi_image = BASE_URL + API + V + '/media/multi';
 }
